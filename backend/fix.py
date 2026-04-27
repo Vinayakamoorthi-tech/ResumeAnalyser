58411fb5c3044a3ab7c66b1f4b247f46c0707517
VinayakamoorthiAV <vinayakapaul@gmail.com>
2026-04-27 18:44:57 +0530

    update render config

--- backend/fix.py
+++
@@ -1,7 +0,0 @@
-# fix_db.py
-import sqlite3
-conn = sqlite3.connect('app.db')
-conn.execute("ALTER TABLE resumes ADD COLUMN created_at DATETIME DEFAULT NULL")
-conn.commit()
-conn.close()
-print('Done')
